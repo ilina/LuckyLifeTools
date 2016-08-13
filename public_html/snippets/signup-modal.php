bbbe1e75bd75932f2143b1698b3ed18772e20342
ilina <ilinavs@gmail.com>
2016-08-12 20:33:56 -0400

    modal columns

--- public_html/snippets/signup-modal.php
+++ public_html/snippets/signup-modal.php
@@ -11,7 +11,7 @@
 	      <div class="col-md-12">
 	        <div class="input-group">
 	          <div class="row">
-	            <div class="one columns">Email:</div> 
+	            <div class="two columns">Email:</div> 
 	            <div class="four columns form-input">
 	              <input type="email" name="email" placeholder="Your Email" required />
 	            </div>
@@ -19,14 +19,14 @@
 	        </div>
 
 	        <div class="row">
-	          <div class="one columns">Password:</div>
+	          <div class="two columns">Password:</div>
 	          <div class="four columns form-input">
 	            <input type="password" name="password" placeholder="Your Password" required />
 	          </div>
 	        </div>
 
 	        <div class="row">
-	          <div class="one columns">Password (Confirm):</div>
+	          <div class="two columns">Password (Confirm):</div>
 	          <div class="four columns form-input">
 	            <input type="password" name="password2" placeholder="Your Password" required />
 	          </div>
